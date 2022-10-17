44686db2726e85a98a062535eec17253e4b69bae
JuanDavidR1 <judbellor@correo.udistrital.edu.co>
2022-10-17 00:35:18 -0500

    Json

--- Maquina_estados_Json.py
+++ Maquina_estados_Json.py
@@ -1,21 +1,20 @@
 #Programa Maquina de estados finitos JSON
 import json
-class FMS_C2:
-                
-    def ejemplos_prueba(a):
-        ejemplos_entrada=        ["0001","0010","0011","0100","0101","0110"]
-        ejemplo_estado_actual=   ["1110","1100","1110","1000","1110","1100"]
-        ejemplo_estado_siguiente=["1111","1110","1111","1100","1111","1110"]
-        ejemplos_salida=         ["1111","1110","1101","1100","1011","1010"]
-        salida=[ejemplos_entrada[a],ejemplo_estado_actual[a],ejemplo_estado_siguiente[a],ejemplos_salida[a]]
-        return salida
 
-Datos = [ {'Estado_actual' : ['0','0','1','1'] }, {'Entrada' : ['0','1','0','1']}, {'Salida' : ['0','1','1','0'] }, {'Estado_siguiente' : ['0','1','1','1']} ] 
+def ejemplos_prueba(a):
+    ejemplos_entrada=        ["0001","0010","0011","0100","0101","0110"]
+    ejemplo_estado_actual=   ["1110","1100","1110","1000","1110","1100"]
+    ejemplo_estado_siguiente=["1111","1110","1111","1100","1111","1110"]
+    ejemplos_salida=         ["1111","1110","1101","1100","1011","1010"]
+    salida=[ejemplos_entrada[a],ejemplo_estado_actual[a],ejemplo_estado_siguiente[a],ejemplos_salida[a]]
+    return salida
+
+"""Datos = [ {'Estado_actual' : ['0','0','1','1'] }, {'Entrada' : ['0','1','0','1']}, {'Salida' : ['0','1','1','0'] }, {'Estado_siguiente' : ['0','1','1','1']} ] 
 print(Datos)
 
 with open('Maquina-estados-finitos-JSON/Datos.json', 'w') as f:  # W es para escritura y f es la variable de manejo
-    json.dump(Datos, f) 
-    #def prueba(entrada_ejemplo,actual_ejemplo,siguiente_ejemplo,salida_ejemplo):
+    json.dump(Datos, f) """
+
 
 #with open('libreria.json', 'r') as archivo:
     #libreria_leida=json.load(archivo)
@@ -38,8 +37,7 @@ with open('Maquina-estados-finitos-JSON/Datos.json', 'w') as f:  # W es para esc
     #    Codigo1.append(cod1)
         
 #print (Codigo1)
-maquina= FMS_C2()
 #for j in range (5):
-b=maquina.ejemplos_prueba(1)
+b=ejemplos_prueba(0)
 print(b)
     #print(entradas[j])
